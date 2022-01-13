b8302998628b73b9c106fc3997ae429da5cdd4e4
Tracy Schuh <flyingphoenix222@gmail.com>
2022-01-13 14:25:59 -0800

    Update sup_index.php

--- sup_index.php
+++ sup_index.php
@@ -27,11 +27,11 @@
 <!DOCTYPE html>
 <html lang= "en">
     <head>
-		<title>SUP Home</title>
-		<meta charset="UTF-8">
+	<title>SUP Home</title>
+	<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/sup.css">
-		<link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/index.css">
+	<link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/index.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://tracyschuh.com/SUP/js/friends.js"></script>
     </head>
