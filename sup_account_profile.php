6c5f8364499afb41313c1709eef717fca5649918
Tracy Schuh <flyingphoenix222@gmail.com>
2022-01-13 14:22:38 -0800

    Update sup_account_profile.php

--- sup_account_profile.php
+++ sup_account_profile.php
@@ -138,10 +138,10 @@
 <!DOCTYPE html>
 <html lang= "en">
     <head>
-		<title>SUP Account Profile</title>
-		<meta charset="UTF-8">
+	<title>SUP Account Profile</title>
+	<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/sup.css">
+	<link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/sup.css">
         <link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/profile_contact.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     </head>
