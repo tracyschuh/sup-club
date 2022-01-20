c4ec001a64e69513a4d1de9c9bfa9382c2aaec52
Tracy Schuh <flyingphoenix222@gmail.com>
2022-01-20 09:41:45 -0800

    Update sup_contact.php

--- sup_contact.php
+++ sup_contact.php
@@ -36,6 +36,7 @@
         <link rel="stylesheet" type="text/css" href="https://tracyschuh.com/SUP/CSS/profile_contact.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+	<script src="https://tracyschuh.com/SUP/js/navbar.js"></script>
     </head>
     <body id = "allbodycontent">
         
